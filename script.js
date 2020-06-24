d42ea4cf539fb58dc98682d427a5ed8d93918123
D-Know <n.dino90@gmail.com>
2020-06-24 11:12:03 -0700

    everything working properly

--- script.js
+++ script.js
@@ -1,44 +1,80 @@
 var today = moment().format('MMMM Do, YYYY')
 var currentDayEl = $('#currentDay')
 var inputEl = $('input')
+//var buttonEl = $('.btn')
+//var hourToDO =
+let workingArray = [];
 currentDayEl.text(today)
 
 var currentHour = moment().format('HH');
 
-console.log(currentHour);
+//console.log(currentHour);
 
 
-// on click save the input elements to local storage using JSON 
-
-// 
-$('.btn').click(function(){
-    console.log('button works');
-    console.log(inputEl.val());
-})
 
 var rowsArray = document.getElementsByClassName('row');
 for(let i = 0; i<rowsArray.length; i++) {
    
     var rowEl = rowsArray[i]
-    let hour = rowEl.getAttribute("id");
+    var hour = rowEl.getAttribute("id");
     hour = hour.split("r");
     hour = hour[hour.length - 1]
-    let colorChange = $(rowEl).find(".colorchange")
+    let input = $(rowEl).find("input")
 
-    console.log(rowEl)
-    console.log(hour)
+    //console.log(rowEl)
+    
 
     if(hour < currentHour) {
-        colorChange.addClass("bg-danger");
+        input.addClass("bg-danger");
     }
     else if(hour === currentHour){
-        colorChange.addClass('bg-info');
+        input.addClass('bg-info');
     }
     else{
-        colorChange.addClass('bg-success');
+        input.addClass('bg-success');
     }
-    
+   
 }
+
+
+
+$(document).on('click','button', function(e) {
+    //console.log($(this));
+    let val = $(this).siblings(".task").val()
+    console.log(val);
+    let parentId = $(this).parent().attr("id")
+    console.log(parentId)
+    workingArray.push(
+        {
+            "text": val, 
+            "id": parentId
+        }
+    )
+    console.log(workingArray)
+    localStorage.setItem('schedule', JSON.stringify(workingArray))
+})
+
+let schedule = JSON.parse(localStorage.getItem('schedule'))
+console.log(schedule);
+for (let i = 0; i < schedule.length; i++) {
+    let text = schedule[i].text; 
+    let id = schedule[i].id; 
+    console.log(text, id)
+    let hourContainer = $(`#${id}`);
+    hourContainer.find('input').val(text)
+}
+/*var buttons = $(":button");
+var inputs = $(":input");
+for(let i=0; i<inputs.length; i++){
+   var inputsArr = inputs[i]
+   var inputNumber = inputsArr.getAttribute("id");
+   var taskToDo = inputs
+   
+   console.log(inputNumber)
+   
+
+}*/
+
 
 
 
